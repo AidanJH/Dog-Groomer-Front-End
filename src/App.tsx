149000b339af43ba96683c93160f447424f974b2
Aidan <aidanjh2012@gmail.com>
2024-02-16 23:06:07 +1100

    Merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {NavBar, Header} from './components'
+import { NavBar, Header } from './components'
 import { Home, Marketplace } from './pages';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
